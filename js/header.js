6f6c20dccbce23e5c452c41b2a1e554fadf97011
Aina <ainaeinikyte@gmail.com>
2024-02-28 11:24:23 +0200

    Cart begins

--- js/header.js
+++ js/header.js
@@ -5,11 +5,12 @@ function header() {
     const href = location.hostname === 'localhost' ? localhost : github;
 
     const HTML = `
-        <a href="${href}">Home</a>
-        <a href="${href}single-button-click">Single button click</a>
-        <a href="${href}counter/">Counter</a>
-        <a href="${href}basketball/">Basketball</a>
-        <a href="${href}virykle/">Virykle</a>
+        <a href="${href}/">Home</a>
+        <a href="${href}/single-button-click">Single button click</a>
+        <a href="${href}/counter/">Counter</a>
+        <a href="${href}/basketball/">Basketball</a>
+        <a href="${href}/virykle/">Virykle</a>
+        <a href="${href}/cart/">Prekiu krepselis</a>
     `;
     navDOM.innerHTML = HTML;
 }
